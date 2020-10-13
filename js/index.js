3be068ca2d4df14c5dcba14eb585e2e024ed75d3
johnkim1025 <63617503+johnkim1025@users.noreply.github.com>
2020-10-13 19:37:03 -0400

    123

--- js/index.js
+++ js/index.js
@@ -8,5 +8,40 @@ $("#tumblr").click(function(){
 $(".ft-hover").hover(function(){
     $(this).css("background-color", "black");
     $(this).css("color", "white");
-    $(this).siblings("background-color", "none");
+});
+$(".ft-hover").on({
+    mouseenter: function () {
+        $(this).css("background-color", "black");
+        $(this).css("color", "white");
+    },
+    mouseleave: function () {
+        $(this).css("background-color", "white");
+        $(this).css("color", "black");
+    }
+});
+
+$("#ayuda-click").on({
+    mouseenter: function () {
+        $('#ayuda-click').css('opacity', '0.90');
+        $('#ayuda-logo-img').hide();
+        $('#ayuda-info').show();
+    },
+    mouseleave: function () {
+        $('#ayuda-click').css('opacity', '1.0');
+        $('#ayuda-logo-img').show();
+        $('#ayuda-info').hide();
+    }
+});
+
+$("#plantp-click").on({
+    mouseenter: function () {
+        $('#plantp-click').css('opacity', '0.90');
+        $('#plantp-logo-img').hide();
+        $('#plantp-info').show();
+    },
+    mouseleave: function () {
+        $('#plantp-click').css('opacity', '1.0');
+        $('#plantp-logo-img').show();
+        $('#plantp-info').hide();
+    }
 });
